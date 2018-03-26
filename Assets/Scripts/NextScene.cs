82c5dcf9602cdbbbad35ce0920b20c9810c48815
amari-a <36706305+amari-a@users.noreply.github.com>
2018-03-26 09:43:28 -0400

    test

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextScene : MonoBehaviour {
+
+    public void ToScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
---
+++ Assets/Scripts/ProgressBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBehavior : MonoBehaviour {
+
+    public Slider progressBar;
+    public float currentScore;
+    float playerScore;
+    float AIscore;
+
+	// Use this for initialization
+	void Start () {
+        playerScore = currentScore / 100;
+        progressBar.value = playerScore;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+
+            progressBar.value += percentValue(5);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+            progressBar.value -= percentValue(5);
+        }
+    }
+
+    float percentValue(float value) { return value / 100; }
+}
